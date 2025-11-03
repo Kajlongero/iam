8835086dac91f4cf9d85bbd4ebf6269531f3010c
Kajlongero <dev.kajlongero@gmail.com>
2025-11-03 18:38:03 -0400

    feat: creating and importing cache module on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from "@nestjs/config";
 
 import { AuthModule } from "./auth/auth.module";
 import { SecurityModule } from "./security/security.module";
+import { CacheModule } from './cache/cache.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ cache: true }), SecurityModule, AuthModule],
+  imports: [ConfigModule.forRoot({ cache: true }), SecurityModule, AuthModule, CacheModule],
   providers: [],
   controllers: [],
 })

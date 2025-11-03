b70a27208f127d867717396242e3c31241b4a870
Kajlongero <dev.kajlongero@gmail.com>
2025-11-03 17:20:25 -0400

    feat: registering iam core itself on db

---
+++ database/data/iam.sql
@@ -0,0 +1,7 @@
+INSERT INTO management.systems (url, client_id, client_secret, redirect_url) VALUES
+(
+  'http://localhost:3000',
+  'IAM_CORE',
+  '$2b$10$ybzb5BueMcTdv5DV.NuHHOvazRvBrD93hffS8ovJUR9ngb6IzvlXu',
+  'http://localhost:3000'
+);

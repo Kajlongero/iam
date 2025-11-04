b84dbff22bf6b0c2b18a006646943421775c3a03
Kajlongero <dev.kajlongero@gmail.com>
2025-11-04 15:10:13 -0400

    feat: cache service where will be all implementations and onModuleInit preloader function

--- src/cache/cache.module.ts
+++ src/cache/cache.module.ts
@@ -1,4 +1,23 @@
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common";
+import { CacheModule as NestCacheModule } from "@nestjs/cache-manager";
 
-@Module({})
+import { PrismaModule } from "src/prisma/prisma.module";
+
+import { CacheService } from "./cache.service";
+import { PreloadRolesService } from "./providers/preload-roles.service";
+import { PreloadSystemsService } from "./providers/preload-systems.service";
+import { PreloadPermissionsService } from "./providers/preload-permissions.service";
+import { PreloadObjectMethodsService } from "./providers/preload-object-methods.service";
+
+@Module({
+  imports: [NestCacheModule, PrismaModule],
+  providers: [
+    CacheService,
+    PreloadRolesService,
+    PreloadSystemsService,
+    PreloadObjectMethodsService,
+    PreloadPermissionsService,
+  ],
+  exports: [NestCacheModule, CacheService],
+})
 export class CacheModule {}
---
+++ src/cache/cache.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, Inject, OnModuleInit } from "@nestjs/common";
+
+import { Cache, CACHE_MANAGER } from "@nestjs/cache-manager";
+
+import { SYSTEMS_TOKEN } from "./tokens";
+
+import { PreloadSystemsService } from "./providers/preload-systems.service";
+import { PreloadObjectMethodsService } from "./providers/preload-object-methods.service";
+import { PreloadPermissionsService } from "./providers/preload-permissions.service";
+
+@Injectable()
+export class CacheService implements OnModuleInit {
+  constructor(
+    @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
+
+    private readonly preloadSystemsService: PreloadSystemsService,
+    private readonly preloadSystemsPermissionsService: PreloadPermissionsService,
+    private readonly preloadSystemsObjectsAndMethodsService: PreloadObjectMethodsService
+  ) {}
+
+  async onModuleInit() {
+    await this.cacheManager.set(SYSTEMS_TOKEN, new Map());
+
+    await this.preloadSystemsService
+      .preload()
+      .then((data) => this.preloadSystemsService.format(data));
+
+    await Promise.all([
+      this.preloadSystemsObjectsAndMethodsService
+        .preload()
+        .then((data) =>
+          this.preloadSystemsObjectsAndMethodsService.format(data)
+        ),
+      this.preloadSystemsPermissionsService
+        .preload()
+        .then((data) => this.preloadSystemsPermissionsService.format(data)),
+    ]);
+  }
+}

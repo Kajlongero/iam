23f1520cd18e57891f6a141e70662d9ecc139dde
Kajlongero <dev.kajlongero@gmail.com>
2025-11-06 16:36:10 -0400

    fix: adding conditions to ensure the permissions are only from the IAM_CORE and not from another system

--- database/data/permissions.users.sql
+++ database/data/permissions.users.sql
@@ -1,13 +1,29 @@
--- Assign all platform permissions (IAM CORE) to SUPERUSER role 
-INSERT INTO 
-  access_control.role_permissions (role_id, permission_id, is_active) 
-SELECT 
-  (SELECT id FROM access_control.roles WHERE name = 'SUPERUSER'), 
-  p.id, 
-  TRUE 
-FROM 
-  access_control.permissions p 
-WHERE 
-  p.name 
-ILIKE 
-  'platform:%';
+-- Assign all platform permissions (IAM CORE UI) to SUPERUSER role
+INSERT INTO
+  access_control.role_permissions (role_id, permission_id, is_active)
+SELECT
+  (
+    SELECT 
+      id 
+    FROM 
+      access_control.roles 
+    WHERE 
+      name = 'SUPERUSER' 
+    AND 
+      system_id = (
+        SELECT 
+          id 
+        FROM 
+          management.systems 
+        WHERE 
+          client_id = 'IAM_CORE_UI'
+      )
+  ),
+  p.id,
+  TRUE
+FROM
+  access_control.permissions p
+WHERE
+  (p.name ILIKE 'platform:%' OR p.name ILIKE 'tenant:%')
+  AND p.system_id = (SELECT id FROM management.systems WHERE client_id = 'IAM_CORE_UI');
+

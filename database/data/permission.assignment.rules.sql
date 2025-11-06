6700b433653806868b271e2485db940019d7b77b
Kajlongero <dev.kajlongero@gmail.com>
2025-11-06 17:02:26 -0400

    feat: formatting and adding more restrictions to ensure data consistency and isolated inserts

--- database/data/permission.assignment.rules.sql
+++ database/data/permission.assignment.rules.sql
@@ -3,52 +3,252 @@ INSERT INTO
   (system_id, minimum_owner_role_id, permission_id)
 VALUES
   (
-    (SELECT id FROM management.systems WHERE client_id = 'IAM_CORE_UI'),
-    (SELECT id FROM access_control.roles WHERE name = 'ADMINISTRATOR'),
-    (SELECT id FROM access_control.permissions WHERE name = 'platform:read_roles')
+    (
+      SELECT id 
+      FROM management.systems 
+      WHERE client_id = 'IAM_CORE_UI'
+    ),
+    (
+      SELECT id 
+      FROM access_control.roles 
+      WHERE name = 'ADMINISTRATOR' AND system_id = (
+        SELECT id 
+        FROM management.systems 
+        WHERE client_id = 'IAM_CORE_UI'
+      )
+    ),
+    (
+      SELECT id 
+      FROM access_control.permissions 
+      WHERE name = 'platform:read_roles' AND system_id = (
+        SELECT id 
+        FROM management.systems 
+        WHERE client_id = 'IAM_CORE_UI'
+      )
+    )
   ),
   (
-    (SELECT id FROM management.systems WHERE client_id = 'IAM_CORE_UI'),
-    (SELECT id FROM access_control.roles WHERE name = 'SUPERADMIN'),
-    (SELECT id FROM access_control.permissions WHERE name = 'platform:create_roles')
+    (
+      SELECT id 
+      FROM management.systems 
+      WHERE client_id = 'IAM_CORE_UI'
+    ),
+    (
+      SELECT id 
+      FROM access_control.roles 
+      WHERE name = 'SUPERADMIN' AND system_id = (
+        SELECT id 
+        FROM management.systems 
+        WHERE client_id = 'IAM_CORE_UI'
+      )
+    ),
+    (
+      SELECT id 
+      FROM access_control.permissions 
+      WHERE name = 'platform:create_roles' AND system_id = (
+        SELECT id 
+        FROM management.systems 
+        WHERE client_id = 'IAM_CORE_UI'
+      )
+    )
   ),
   (
-    (SELECT id FROM management.systems WHERE client_id = 'IAM_CORE_UI'),
-    (SELECT id FROM access_control.roles WHERE name = 'SUPERUSER'),
-    (SELECT id FROM access_control.permissions WHERE name = 'platform:update_roles')
+    (
+      SELECT id 
+      FROM management.systems 
+      WHERE client_id = 'IAM_CORE_UI'
+    ),
+    (
+      SELECT id 
+      FROM access_control.roles 
+      WHERE name = 'SUPERUSER' AND system_id = (
+        SELECT id 
+        FROM management.systems 
+        WHERE client_id = 'IAM_CORE_UI'
+      )
+    ),
+    (
+      SELECT id 
+      FROM access_control.permissions 
+      WHERE name = 'platform:update_roles' AND system_id = (
+        SELECT id 
+        FROM management.systems 
+        WHERE client_id = 'IAM_CORE_UI'
+      )
+    )
   ),
   (
-    (SELECT id FROM management.systems WHERE client_id = 'IAM_CORE_UI'),
-    (SELECT id FROM access_control.roles WHERE name = 'SUPERUSER'),
-    (SELECT id FROM access_control.permissions WHERE name = 'platform:delete_roles')
+    (
+      SELECT id 
+      FROM management.systems 
+      WHERE client_id = 'IAM_CORE_UI'
+    ),
+    (
+      SELECT id 
+      FROM access_control.roles 
+      WHERE name = 'SUPERUSER' AND system_id = (
+        SELECT id 
+        FROM management.systems 
+        WHERE client_id = 'IAM_CORE_UI'
+      )
+    ),
+    (
+      SELECT id 
+      FROM access_control.permissions 
+      WHERE name = 'platform:delete_roles' AND system_id = (
+        SELECT id 
+        FROM management.systems 
+        WHERE client_id = 'IAM_CORE_UI'
+      )
+    )
   ),
   (
-    (SELECT id FROM management.systems WHERE client_id = 'IAM_CORE_UI'),
-    (SELECT id FROM access_control.roles WHERE name = 'ADMINISTRATOR'),
-    (SELECT id FROM access_control.permissions WHERE name = 'platform:read_permissions')
+    (
+      SELECT id 
+      FROM management.systems 
+      WHERE client_id = 'IAM_CORE_UI'
+    ),
+    (
+      SELECT id 
+      FROM access_control.roles 
+      WHERE name = 'ADMINISTRATOR' AND system_id = (
+        SELECT id 
+        FROM management.systems 
+        WHERE client_id = 'IAM_CORE_UI'
+      )
+    ),
+    (
+      SELECT id 
+      FROM access_control.permissions 
+      WHERE name = 'platform:read_permissions' AND system_id = (
+        SELECT id 
+        FROM management.systems 
+        WHERE client_id = 'IAM_CORE_UI'
+      )
+    )
   ),
   (
-    (SELECT id FROM management.systems WHERE client_id = 'IAM_CORE_UI'),
-    (SELECT id FROM access_control.roles WHERE name = 'SUPERUSER'),
-    (SELECT id FROM access_control.permissions WHERE name = 'platform:create_permissions')
+    (
+      SELECT id 
+      FROM management.systems 
+      WHERE client_id = 'IAM_CORE_UI'
+    ),
+    (
+      SELECT id 
+      FROM access_control.roles 
+      WHERE name = 'SUPERUSER' AND system_id = (
+        SELECT id 
+        FROM management.systems 
+        WHERE client_id = 'IAM_CORE_UI'
+      )
+    ),
+    (
+      SELECT id 
+      FROM access_control.permissions 
+      WHERE name = 'platform:create_permissions' AND system_id = (
+        SELECT id 
+        FROM management.systems 
+        WHERE client_id = 'IAM_CORE_UI'
+      )
+    )
   ),
   (
-    (SELECT id FROM management.systems WHERE client_id = 'IAM_CORE_UI'),
-    (SELECT id FROM access_control.roles WHERE name = 'SUPERUSER'),
-    (SELECT id FROM access_control.permissions WHERE name = 'platform:update_permissions')
+    (
+      SELECT id 
+      FROM management.systems 
+      WHERE client_id = 'IAM_CORE_UI'
+    ),
+    (
+      SELECT id 
+      FROM access_control.roles 
+      WHERE name = 'SUPERUSER' AND system_id = (
+        SELECT id 
+        FROM management.systems 
+        WHERE client_id = 'IAM_CORE_UI'
+      )
+    ),
+    (
+      SELECT id 
+      FROM access_control.permissions 
+      WHERE name = 'platform:update_permissions' AND system_id = (
+        SELECT id 
+        FROM management.systems 
+        WHERE client_id = 'IAM_CORE_UI'
+      )
+    )
   ),
   (
-    (SELECT id FROM management.systems WHERE client_id = 'IAM_CORE_UI'),
-    (SELECT id FROM access_control.roles WHERE name = 'SUPERUSER'),
-    (SELECT id FROM access_control.permissions WHERE name = 'platform:delete_permissions')
+    (
+      SELECT id 
+      FROM management.systems 
+      WHERE client_id = 'IAM_CORE_UI'
+    ),
+    (
+      SELECT id 
+      FROM access_control.roles 
+      WHERE name = 'SUPERUSER' AND system_id = (
+        SELECT id 
+        FROM management.systems 
+        WHERE client_id = 'IAM_CORE_UI'
+      )
+    ),
+    (
+      SELECT id 
+      FROM access_control.permissions 
+      WHERE name = 'platform:delete_permissions' AND system_id = (
+        SELECT id 
+        FROM management.systems 
+        WHERE client_id = 'IAM_CORE_UI'
+      )
+    )
   ),
   (
-    (SELECT id FROM management.systems WHERE client_id = 'IAM_CORE_UI'),
-    (SELECT id FROM access_control.roles WHERE name = 'SUPERADMIN'),
-    (SELECT id FROM access_control.permissions WHERE name = 'platform:assign_permissions')
+    (
+      SELECT id 
+      FROM management.systems 
+      WHERE client_id = 'IAM_CORE_UI'
+    ),
+    (
+      SELECT id 
+      FROM access_control.roles 
+      WHERE name = 'SUPERADMIN' AND system_id = (
+        SELECT id 
+        FROM management.systems 
+        WHERE client_id = 'IAM_CORE_UI'
+      )
+    ),
+    (
+      SELECT id 
+      FROM access_control.permissions 
+      WHERE name = 'platform:assign_permissions' AND system_id = (
+        SELECT id 
+        FROM management.systems 
+        WHERE client_id = 'IAM_CORE_UI'
+      )
+    )
   ),
   (
-    (SELECT id FROM management.systems WHERE client_id = 'IAM_CORE_UI'),
-    (SELECT id FROM access_control.roles WHERE name = 'SUPERUSER'),
-    (SELECT id FROM access_control.permissions WHERE name = 'platform:revoke_permissions')
+    (
+      SELECT id 
+      FROM management.systems 
+      WHERE client_id = 'IAM_CORE_UI'
+    ),
+    (
+      SELECT id 
+      FROM access_control.roles 
+      WHERE name = 'SUPERUSER' AND system_id = (
+        SELECT id 
+        FROM management.systems 
+        WHERE client_id = 'IAM_CORE_UI'
+      )
+    ),
+    (
+      SELECT id 
+      FROM access_control.permissions 
+      WHERE name = 'platform:revoke_permissions' AND system_id = (
+        SELECT id 
+        FROM management.systems 
+        WHERE client_id = 'IAM_CORE_UI'
+      )
+    )
   );

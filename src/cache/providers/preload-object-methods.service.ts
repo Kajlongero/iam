6c4237b6e43018c481e9726062f7736295fad36f
Kajlongero <dev.kajlongero@gmail.com>
2025-11-16 13:08:59 -0400

    refactor: preload-object-methods service to correct save on cache data

--- src/cache/providers/preload-object-methods.service.ts
+++ src/cache/providers/preload-object-methods.service.ts
@@ -1,75 +1,92 @@
-import { Inject, Injectable } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 
-import { Cache, CACHE_MANAGER } from "@nestjs/cache-manager";
-
-import { SYSTEMS_TOKEN } from "../tokens";
-
+import { RedisService } from "src/redis/redis.service";
 import { PrismaService } from "src/prisma/prisma.service";
+import { CacheKeysService } from "./cache-keys.service";
 
-import type { Systems } from "generated/prisma";
 import type { CachePreloader } from "../interfaces/preloaders.interface";
-import type { IAM, IObjects, IMethods } from "../interfaces/cache.interface";
 
-interface IObjectMethodSystemScope extends IObjects {
-  method: IMethods[];
-  system: Pick<Systems, "clientId">;
+import type { Application } from "generated/prisma";
+import type { HashKeyValue } from "src/redis/interfaces/hash.interface";
+
+import type { IObjectMethod } from "../interfaces/objects.interface";
+
+interface IApplicationObjectMethod extends IObjectMethod {
+  application: Pick<Application, "clientId">;
 }
 
 @Injectable()
 export class PreloadObjectMethodsService
-  implements CachePreloader<IObjectMethodSystemScope>
+  implements CachePreloader<IApplicationObjectMethod>
 {
   constructor(
-    @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
-
-    private readonly prisma: PrismaService
+    private readonly prisma: PrismaService,
+    private readonly redisService: RedisService,
+    private readonly cacheKeysService: CacheKeysService
   ) {}
 
-  async preload(): Promise<IObjectMethodSystemScope[]> {
-    const data = await this.prisma.objects.findMany({
-      select: {
-        id: true,
-        name: true,
-        isActive: true,
-        description: true,
-        method: {
-          select: {
-            id: true,
-            name: true,
-            isActive: true,
-            description: true,
-          },
-        },
-        system: {
+  async preload(): Promise<IApplicationObjectMethod[]> {
+    const data = await this.prisma.object.findMany({
+      include: {
+        application: {
           select: {
             clientId: true,
           },
         },
+        methods: true,
       },
     });
 
-    return data as unknown as IObjectMethodSystemScope[];
+    return data;
   }
 
-  async format(data: IObjectMethodSystemScope[]): Promise<void> {
-    const systems = await this.cacheManager.get<IAM>(SYSTEMS_TOKEN);
+  format<J>(data: IApplicationObjectMethod[]): J {
+    const map = new Map();
 
-    data.forEach((o) => {
-      const { id, name, isActive, description, method, system } = o;
+    data.forEach((item) => {
+      const { application, methods, ...rest } = item;
 
-      const systemMap = systems?.get(system.clientId);
-      if (!systemMap) return;
+      const key = this.cacheKeysService.getApplicationsObjectKey(
+        application.clientId,
+        rest.name
+      );
 
-      const objectsAndMethods = systemMap.objects;
-      if (!objectsAndMethods) systemMap.objects = new Map();
+      const obj = {
+        __meta: JSON.stringify(rest),
+      };
 
-      objectsAndMethods.set(name, {
-        id,
-        name,
-        isActive,
-        description,
-        methods: new Map(method.map((m) => [m.name, m])),
+      methods.forEach((method) => {
+        const keyName = this.cacheKeysService.getApplicationsObjectMethodKey(
+          application.clientId,
+          rest.name,
+          method.name
+        );
+
+        obj[keyName] = JSON.stringify(method);
       });
+
+      if (!map.has(key)) map.set(key, obj);
     });
+
+    return Array.from(map.entries()).map(([key, value]) => {
+      const values = Object.entries(value as Record<string, unknown>);
+      const keyName = key as string;
+
+      return {
+        key: keyName,
+        values: values.map(([fieldKey, fieldValue]) => ({
+          key: fieldKey,
+          value: fieldValue,
+        })),
+      };
+    }) as J;
+  }
+
+  async save<T>(args: T): Promise<void> {
+    const pipeline = this.redisService.pipeline();
+
+    this.redisService.mhsetToPipeline(pipeline, args as HashKeyValue[]);
+
+    await pipeline.exec();
   }
 }

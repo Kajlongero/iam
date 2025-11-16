cecd245ca45b35afddd8ab745aa49db6ae736791
Kajlongero <dev.kajlongero@gmail.com>
2025-11-16 13:09:24 -0400

    fix: using hashes to save data and accessing them faster

--- src/cache/providers/preload-applications.service.ts
+++ src/cache/providers/preload-applications.service.ts
@@ -4,9 +4,8 @@ import { RedisService } from "src/redis/redis.service";
 import { PrismaService } from "src/prisma/prisma.service";
 import { CacheKeysService } from "./cache-keys.service";
 
-import type { KeyValue } from "src/redis/interfaces/key-val.interface";
-
 import type { Application } from "generated/prisma";
+import type { HashKeyValue } from "src/redis/interfaces/hash.interface";
 import type { CachePreloader } from "../interfaces/preloaders.interface";
 
 @Injectable()
@@ -18,40 +17,33 @@ export class PreloadApplicationsService implements CachePreloader<Application> {
     private readonly cacheKeysService: CacheKeysService
   ) {}
 
-  async preload(): Promise<Application[]> {
+  async preload<T>(): Promise<T[]> {
     const applications = await this.prisma.application.findMany({});
 
-    return applications;
+    return applications as T[];
   }
 
-  format(data: Application[]): KeyValue[] | Application[] {
-    const mapped: KeyValue[] = data.map((app) => {
-      return {
-        key: this.cacheKeysService.getApplicationsKey(app.clientId),
-        value: JSON.stringify({
-          id: app.id,
-          name: app.name,
-          clientId: app.clientId,
-          clientSecret: app.clientSecret,
-          statusId: app.statusId,
-          creatorUserId: app.creatorUserId,
-          createdAt: app.createdAt,
-          deletedAt: app.deletedAt,
-        }),
-      };
-    });
-
-    const globalApplications: KeyValue = {
+  format<J>(data: Application[]): J {
+    const map: HashKeyValue = {
       key: this.cacheKeysService.getGlobalApplicationsKey(),
-      value: JSON.stringify(data),
+      values: [],
     };
 
-    mapped.push(globalApplications);
+    data.forEach((app) => {
+      map.values.push({
+        key: this.cacheKeysService.getApplicationsKey(app.clientId),
+        value: JSON.stringify(app),
+      });
+    });
 
-    return mapped;
+    return map as J;
   }
 
-  async save(args: KeyValue[]): Promise<void> {
-    await this.redisService.pipeline(args);
+  async save<T>(args: T): Promise<void> {
+    const pipeline = this.redisService.pipeline();
+
+    this.redisService.hsetToPipeline(pipeline, args as HashKeyValue);
+
+    await pipeline.exec();
   }
 }

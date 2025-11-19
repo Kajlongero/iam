c6e062dd02d3e5c8f48d7bca7f8551fd699309e2
Kajlongero <dev.kajlongero@gmail.com>
2025-11-18 21:39:08 -0400

    feat: preload resource server permissions (S2S)

---
+++ src/cache/providers/preload-resource-server-permissions.service.ts
@@ -0,0 +1,117 @@
+import { Injectable } from "@nestjs/common";
+
+import { RedisService } from "src/redis/redis.service";
+import { PrismaService } from "src/prisma/prisma.service";
+import { CacheKeysService } from "./cache-keys.service";
+
+import type { CachePreloader } from "../interfaces/preloaders.interface";
+
+import type { HashKeyValue } from "src/redis/interfaces/hash.interface";
+import type { Application, Permission, Role } from "generated/prisma";
+
+export interface IApiPermissionRules extends Permission {
+  application?: Pick<Application, "clientId"> | null;
+  permissionAssignmentRules: {
+    role: Pick<Role, "id" | "name">;
+  }[];
+}
+
+@Injectable()
+export class PreloadApiPermissionsService
+  implements CachePreloader<IApiPermissionRules>
+{
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly redisService: RedisService,
+    private readonly cacheKeysService: CacheKeysService
+  ) {}
+
+  async preload(): Promise<IApiPermissionRules[]> {
+    const data = await this.prisma.permission.findMany({
+      where: {
+        isApiScope: true,
+      },
+      include: {
+        application: {
+          select: {
+            clientId: true,
+          },
+        },
+        permissionAssignmentRules: {
+          include: {
+            role: {
+              select: {
+                id: true,
+                name: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    return data as IApiPermissionRules[];
+  }
+
+  format<J>(data: IApiPermissionRules[]): J {
+    const map = new Map();
+
+    const globalKey =
+      this.cacheKeysService.getGlobalApiPermissionsMetadataKey();
+
+    map.set(globalKey, {});
+
+    data.forEach((p) => {
+      const { application, permissionAssignmentRules, ...permission } = p;
+
+      if (!application?.clientId) {
+        const record = map.get(globalKey) as Record<string, string>;
+
+        const key = permission.name;
+
+        const res: IApiPermissionRules = {
+          ...permission,
+          permissionAssignmentRules,
+        };
+
+        record[key] = JSON.stringify(res);
+      } else {
+        const element = this.cacheKeysService.getApplicationsApiPermissionsKey(
+          application.clientId
+        );
+
+        const record = map.get(element) as Record<string, string>;
+
+        const key = permission.name;
+
+        const res: IApiPermissionRules = {
+          ...permission,
+          permissionAssignmentRules,
+        };
+
+        record[key] = JSON.stringify(res);
+      }
+    });
+
+    return Array.from(map.entries()).map(([key, value]) => {
+      const hashKey = key as string;
+      const object = value as Record<string, string>;
+
+      return {
+        key: hashKey,
+        values: Object.entries(object).map(([fieldKey, fieldValue]) => ({
+          key: fieldKey,
+          value: fieldValue,
+        })),
+      };
+    }) as J;
+  }
+
+  async save<J>(data: J): Promise<void> {
+    const pipeline = this.redisService.pipeline();
+
+    this.redisService.mhsetToPipeline(pipeline, data as HashKeyValue[]);
+
+    await pipeline.exec();
+  }
+}

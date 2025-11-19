34f757c626d635f1455d3cd1f0012d3ce4a30e05
Kajlongero <dev.kajlongero@gmail.com>
2025-11-19 13:22:33 -0400

    fix: renaming decorator and adding contants to not make magic strings on code

--- src/security/decorators/object-method.decorator.ts
+++ src/security/decorators/object-method.decorator.ts
@@ -1,8 +1,9 @@
 import { SetMetadata } from "@nestjs/common";
 
 export const IAMObjectKey = "IAM_OBJECT_KEY";
+
 export const IAMMethodKey = "IAM_METHOD_KEY";
 
-export const Object = (...args: string[]) => SetMetadata(IAMObjectKey, args);
+export const IAMObject = (...args: string[]) => SetMetadata(IAMObjectKey, args);
 
-export const Method = (...args: string[]) => SetMetadata(IAMMethodKey, args);
+export const IAMMethod = (...args: string[]) => SetMetadata(IAMMethodKey, args);

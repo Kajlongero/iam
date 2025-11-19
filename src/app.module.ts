07f526a3f37cbbf031600e14d378ed353f71209e
Kajlongero <dev.kajlongero@gmail.com>
2025-11-19 13:25:23 -0400

    feat: importing on app module the iam consistency module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,27 @@
 import { Module } from "@nestjs/common";
+import { APP_GUARD } from "@nestjs/core";
 import { ConfigModule } from "@nestjs/config";
 
 import { AuthModule } from "./auth/auth.module";
+import { CacheModule } from "./cache/cache.module";
 import { SecurityModule } from "./security/security.module";
-import { CacheModule } from './cache/cache.module';
+
+import { ValidateObjectMethodAccessGuard } from "./security/guards/validate-object-method-access.guard";
+import { IamConsistencyModule } from "./iam-consistency/iam-consistency.module";
 
 @Module({
-  imports: [ConfigModule.forRoot({ cache: true }), SecurityModule, AuthModule, CacheModule],
-  providers: [],
-  controllers: [],
+  imports: [
+    ConfigModule.forRoot({ cache: true }),
+    AuthModule,
+    CacheModule,
+    SecurityModule,
+    IamConsistencyModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ValidateObjectMethodAccessGuard,
+    },
+  ],
 })
 export class AppModule {}

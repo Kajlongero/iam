a49fd329020353a355978f4890eede5905b93674
Kajlongero <dev.kajlongero@gmail.com>
2025-11-20 11:28:01 -0400

    feat: importing cacge module on auth module to use relevant guards

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,13 +3,15 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 
 import { PrismaModule } from "src/prisma/prisma.module";
+import { CacheModule } from "src/cache/cache.module";
 import { SecurityModule } from "src/security/security.module";
 
 import { AuthService } from "./auth.service";
 
 import { AuthController } from "./auth.controller";
+
 @Module({
-  imports: [ConfigModule, PrismaModule, SecurityModule],
+  imports: [ConfigModule, PrismaModule, CacheModule, SecurityModule],
   exports: [AuthService],
   providers: [AuthService],
   controllers: [AuthController],

6378552eba4bfb5bef6d0a7f908576cfd5778f38
Kajlongero <dev.kajlongero@gmail.com>
2025-11-20 00:35:24 -0400

    changes: deleting all cache methods on cache service to only use the methods from redisService

--- src/cache/cache.service.ts
+++ src/cache/cache.service.ts
@@ -1,7 +1,5 @@
 import { Injectable, OnModuleInit } from "@nestjs/common";
 
-import type { Callback } from "ioredis";
-
 import { RedisService } from "src/redis/redis.service";
 import { PreloadRolesService } from "./providers/preload-roles.service";
 import { PreloadApplicationsService } from "./providers/preload-applications.service";
@@ -9,10 +7,8 @@ import { PreloadObjectMethodsService } from "./providers/preload-object-methods.
 import { PreloadApiPermissionsService } from "./providers/preload-resource-server-permissions.service";
 import { PreloadUserPermissionsService } from "./providers/preload-user-permissions.service";
 
-import type { CacheGetterSetters } from "./interfaces/cache-getters-setters.interface";
-
 @Injectable()
-export class CacheService implements OnModuleInit, CacheGetterSetters {
+export class CacheService implements OnModuleInit {
   constructor(
     private readonly redisService: RedisService,
     private readonly preloadRolesService: PreloadRolesService,
@@ -30,47 +26,5 @@ export class CacheService implements OnModuleInit, CacheGetterSetters {
       this.preloadApiPermissionsService.preload(),
       this.preloadUserPermissionsService.preload(),
     ]);
-  }
-
-  async exists(key: string | string[]): Promise<number> {
-    const data = await this.redisService.exists(key);
-
-    return data;
-  }
-
-  async hexists(hash: string, key: string): Promise<number> {
-    const data = await this.redisService.hexists(hash, key);
-
-    return data;
-  }
-
-  async get<T>(key: string, cb?: Callback<string | null>): Promise<T> {
-    const data = await this.redisService.get<T>(key, cb);
-
-    return data;
-  }
-
-  async mget<T>(keys: string[]): Promise<T[]> {
-    const data = await this.redisService.mget<T>(keys);
-
-    return data as T[];
-  }
-
-  async hget<T>(key: string, field: string, cb?: Callback<string | null>) {
-    const data = await this.redisService.hget<T>(key, field, cb);
-
-    return data;
-  }
-
-  async hmget<T>(key: string, fields: string[]) {
-    const data = await this.redisService.hmget<T>(key, fields);
-
-    return data;
-  }
-
-  async hgetall<T>(key: string) {
-    const data = await this.redisService.hgetall<T>(key);
-
-    return data;
   }
 }

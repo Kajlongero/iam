c6c96c447925624445b5ccb086b545acd7b4e595
Kajlongero <dev.kajlongero@gmail.com>
2025-11-21 01:47:17 -0400

    feat: auth controller and service skeletons

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,24 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Controller, Post, Body, UseGuards } from "@nestjs/common";
+
 import { AuthService } from "./auth.service";
+
+import { BruteForceGuard } from "src/security/guards/brute-force.guard";
+
 import { LoginDto } from "./dtos/login-user.dto";
+import { RegisterDto } from "./dtos/register-user.dto";
 
 @Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post("login")
+  @UseGuards(BruteForceGuard)
   login(@Body() payload: LoginDto) {
     return this.authService.login(payload);
+  }
+
+  @Post("register")
+  register(@Body() payload: RegisterDto) {
+    return this.authService.register(payload);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,31 @@
-import { Body, Injectable } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
+import { Inject, Injectable } from "@nestjs/common";
+
+import { JWT_TOKEN_PROVIDERS } from "src/security/constants/provider-tokens.constants";
+
 import { PrismaService } from "src/prisma/prisma.service";
+
 import { LoginDto } from "./dtos/login-user.dto";
+import { RegisterDto } from "./dtos/register-user.dto";
+import { RedisService } from "src/redis/redis.service";
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    @Inject(JWT_TOKEN_PROVIDERS.ACCESS_TOKEN_PROVIDER)
+    private readonly accessJwtService: JwtService,
+    @Inject(JWT_TOKEN_PROVIDERS.REFRESH_TOKEN_PROVIDER)
+    private readonly refreshJwtService: JwtService,
 
-  login(@Body() body: LoginDto) {
-    return body;
+    private readonly prisma: PrismaService,
+    private readonly redisService: RedisService,
+    private readonly configService: ConfigService
+  ) {}
+
+  login(payload: LoginDto) {
+    const { email, password } = payload;
   }
+
+  async register(payload: RegisterDto) {}
 }

74915629f81d096da1b67a0634b9dc4ad925e93c
Kajlongero <dev.kajlongero@gmail.com>
2025-11-21 16:15:31 -0400

    feat: get ip brute force status lua script
    fix: get rs via mapped name, fixed GET to HGET because that key is a hash and the clientId is the term to search for in the hash

--- src/cache/constants/lua-scripts.constants.ts
+++ src/cache/constants/lua-scripts.constants.ts
@@ -1,11 +1,45 @@
-export const GET_RS_VIA_MAPPED_NAME = `
-local appId = redis.call('GET', KEYS[1])
+export const GET_IP_BRUTE_FORCE_STATUS = `
+-- KEYS[1]: iam:security:rate-limit:blocked:ip
+-- KEYS[2]: iam:security:rate-limit:attempts:ip
+-- ARGV[1]: MAX_ATTEMPTS
+-- ARGV[2]: TIME_WINDOW
+-- ARGV[3]: BLOCK_TIME
 
-if not appId then
+local isBlocked = redis.call('GET', KEYS[1])
+
+if isBlocked then 
+  return redis.call('TTL', KEYS[1])
+end
+
+local attempts = redis.call('INCR', KEYS[2])
+
+if attempts == 1 then
+  redis.call('EXPIRE', KEYS[2], ARGV[2])
+end
+
+if attempts > tonumber(ARGV[1]) then
+  redis.call('UNLINK', KEYS[2])
+
+  redis.call('SET', KEYS[1], 1, 'EX', ARGV[3])
+
+  return tonumber(ARGV[3])
+end
+
+return 0
+`;
+
+export const GET_RS_VIA_MAPPED_NAME = `
+-- KEYS[1]: iam:global:resource_server_names
+-- ARGV[1]: clientId
+
+-- It retrieves application clientId
+local appClientId = redis.call('HGET', KEYS[1], ARGV[1])
+
+if not appClientId then
   return nil
 end
 
-local appHashKey = 'iam:application:' .. appId .. ':resource_servers'
+local appHashKey = 'iam:application:' .. appClientId .. ':resource_servers'
 
 return redis.call('HGET', appHashKey, ARGV[1])
 `;

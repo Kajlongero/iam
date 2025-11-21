c87d070912ba0bc4c373935bce094014d6c764ca
Kajlongero <dev.kajlongero@gmail.com>
2025-11-21 18:22:32 -0400

    feat: using lua script to handling brute force on redis

--- src/security/guards/brute-force.guard.ts
+++ src/security/guards/brute-force.guard.ts
@@ -8,10 +8,11 @@ import {
   CanActivate,
   HttpException,
   ExecutionContext,
-  ServiceUnavailableException,
 } from "@nestjs/common";
 
+import { HTTP_RESPONSES } from "src/commons/responses/http.responses";
 import { RATE_LIMIT_CONSTANTS } from "../constants/rate-limit.constants";
+import { GET_IP_BRUTE_FORCE_STATUS } from "src/cache/constants/lua-scripts.constants";
 
 import { RedisService } from "src/redis/redis.service";
 import { CacheKeysService } from "src/cache/providers/cache-keys.service";
@@ -20,27 +21,23 @@ import { CacheKeysService } from "src/cache/providers/cache-keys.service";
 export class BruteForceGuard implements CanActivate {
   private readonly logger = new Logger(BruteForceGuard.name);
 
-  private readonly BLOCK_TIME: number;
-  private readonly WINDOW_TIME: number;
-  private readonly MAX_ATTEMPTS: number;
+  private readonly BLOCK_TIME: string;
+  private readonly WINDOW_TIME: string;
+  private readonly MAX_ATTEMPTS: string;
 
   constructor(
     private readonly cfgService: ConfigService,
     private readonly redisService: RedisService,
     private readonly cacheKeysService: CacheKeysService
   ) {
-    this.BLOCK_TIME = parseInt(
-      this.cfgService.getOrThrow(
-        RATE_LIMIT_CONSTANTS.RATE_LIMIT_BLOCK_TIME_SECONDS
-      )
+    this.BLOCK_TIME = this.cfgService.getOrThrow(
+      RATE_LIMIT_CONSTANTS.RATE_LIMIT_BLOCK_TIME_SECONDS
     );
-    this.WINDOW_TIME = parseInt(
-      this.cfgService.getOrThrow(
-        RATE_LIMIT_CONSTANTS.RATE_LIMIT_WINDOW_TIME_SECONDS
-      )
+    this.WINDOW_TIME = this.cfgService.getOrThrow(
+      RATE_LIMIT_CONSTANTS.RATE_LIMIT_WINDOW_TIME_SECONDS
     );
-    this.MAX_ATTEMPTS = parseInt(
-      this.cfgService.getOrThrow(RATE_LIMIT_CONSTANTS.RATE_LIMIT_MAX_ATTEMPTS)
+    this.MAX_ATTEMPTS = this.cfgService.getOrThrow(
+      RATE_LIMIT_CONSTANTS.RATE_LIMIT_MAX_ATTEMPTS
     );
   }
 
@@ -56,88 +53,32 @@ export class BruteForceGuard implements CanActivate {
 
     const sanitizedIp = this.sanitizeIp(ip);
 
-    const blockKey = this.cacheKeysService.getBlockedClientsByIp(sanitizedIp);
-    const attemptsKey =
-      this.cacheKeysService.getAttemptsOfClientByIp(sanitizedIp);
+    const block = this.cacheKeysService.getBlockedClientsByIp(sanitizedIp);
+    const attempts = this.cacheKeysService.getAttemptsOfClientByIp(sanitizedIp);
 
-    await this.isBlocked(blockKey);
+    const isBlocked = await this.redisService.eval(
+      GET_IP_BRUTE_FORCE_STATUS,
+      2,
+      [block, attempts, this.MAX_ATTEMPTS, this.WINDOW_TIME, this.BLOCK_TIME]
+    );
 
-    await this.checkAttempts(attemptsKey, blockKey, sanitizedIp);
+    if (isBlocked !== 0)
+      return this.throwTooManyRequestsError(isBlocked as number);
 
     return true;
-  }
-
-  private async isBlocked(blockKey: string) {
-    const pipeline = this.redisService.pipeline();
-
-    pipeline.get(blockKey);
-    pipeline.ttl(blockKey);
-
-    const results = await pipeline.exec();
-    if (!results) throw new ServiceUnavailableException();
-
-    const [[blockErr, isBlocked], [ttlErr, ttl]] = results;
-
-    if (ttlErr || blockErr) throw new ServiceUnavailableException();
-
-    if (isBlocked) {
-      const timeLeft = typeof ttl === "number" && ttl > 0 ? ttl : 60;
-      this.throwTooManyRequestsError(timeLeft);
-    }
-  }
-
-  private async checkAttempts(
-    attemptsKey: string,
-    blockKey: string,
-    ip: string
-  ) {
-    const pipeline = this.redisService.pipeline();
-
-    pipeline.incr(attemptsKey);
-    pipeline.ttl(attemptsKey);
-
-    const results = await pipeline.exec();
-    if (!results) throw new ServiceUnavailableException();
-
-    const [[incrErr, attempts], [, ttl]] = results;
-
-    if (incrErr) throw new ServiceUnavailableException();
-
-    const currentAttempts = attempts as number;
-    const currentTtl = ttl as number;
-
-    if (currentAttempts === 1 || currentTtl === -1)
-      await this.redisService.expire(attemptsKey, this.WINDOW_TIME);
-
-    if (currentAttempts > this.MAX_ATTEMPTS)
-      await this.blockIp(attemptsKey, blockKey, ip);
-  }
-
-  private async blockIp(attemptsKey: string, blockKey: string, ip: string) {
-    const blockPipeline = this.redisService.pipeline();
-
-    blockPipeline.unlink(attemptsKey).set(blockKey, 1, "EX", this.BLOCK_TIME);
-
-    await this.redisService.execPipeline(blockPipeline);
-
-    this.logger.warn(`🚨 IP ${ip} blocked due to brute force.`);
-
-    this.throwTooManyRequestsError(this.BLOCK_TIME);
-  }
-
-  private sanitizeIp(ip: string): string {
-    return ip.replace(/:/g, "_");
   }
 
   private throwTooManyRequestsError(retryAfter: number): never {
     throw new HttpException(
       {
-        statusCode: HttpStatus.TOO_MANY_REQUESTS,
-        message: "Too Many Requests. Blocked temporarily",
-        errorCode: "SECURITY_IP_BLOCKED",
+        ...HTTP_RESPONSES[429].BRUTE_FORCE_TOO_MANY_REQUESTS,
         retryAfterSeconds: retryAfter,
       },
       HttpStatus.TOO_MANY_REQUESTS
     );
+  }
+
+  private sanitizeIp(ip: string): string {
+    return ip.replace(/:/g, "_");
   }
 }

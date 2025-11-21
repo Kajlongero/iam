b0d892fe3063a0b303fac07607832f485a1b53de
Kajlongero <dev.kajlongero@gmail.com>
2025-11-21 18:25:23 -0400

    feat: resource disabled object

--- src/commons/responses/http.responses.ts
+++ src/commons/responses/http.responses.ts
@@ -1,4 +1,11 @@
 export const HTTP_RESPONSES = {
+  403: {
+    RESOURCE_DISABLED_FORBIDDEN: {
+      statusCode: 403,
+      message: "This resource is currently disabled by administrator",
+      errorCode: "RESOURCE_DISABLED",
+    },
+  },
   429: {
     BRUTE_FORCE_TOO_MANY_REQUESTS: {
       statusCode: 429,

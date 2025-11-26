a4e065624aac25884874f15db21b99e4a32c6be6
Kajlongero <dev.kajlongero@gmail.com>
2025-11-25 23:54:14 -0400

    feat: adding mock slugs

--- database/seeds/10.resource.servers.sql
+++ database/seeds/10.resource.servers.sql
@@ -1,6 +1,7 @@
 INSERT INTO
   management.resource_servers (
     name,
+    slug,
     client_id,
     client_secret,
     application_id,
@@ -9,6 +10,7 @@ INSERT INTO
 VALUES
   (
     'iam-system-api',
+    'iam-syste-api_f5981a16c78b035d',
     'IAM_SYSTEM_API_CLIENT_ID',
     '$argon2id$v=19$m=256,t=1,p=1$MPgnCKnte28wZpQOLS4WTQ$92TLWxBmm0euOZmaoJIUDTckDrrnkc2Q3UDebbYhqaU',
     (
@@ -23,6 +25,7 @@ VALUES
   ),
   (
     'iam-ui-backend',
+    'iam-ui-backend_c2bcab70391b1eb3',
     'IAM_UI_BACKEND_CLIENT_ID',
     '$argon2id$v=19$m=256,t=1,p=1$bvhRFEeB2Cqz1d9vhxPYCg$WJibbch/3BStozC2xXCcpdZ2axJ9j1ICvZkxX4eYr4Y',
     (
@@ -37,6 +40,7 @@ VALUES
   ),
   (
     'orders-service',
+    'orders-service_260dd00062ad0354',
     'FOOD_APP_ORDER_SERVICE_CLIENT_ID',
     '$argon2id$v=19$m=256,t=1,p=1$QYaK+QTJHBdu+xS6Bq6IvQ$nbOYt9n+sbUtqat7qKSHOXc2dE42Bhtq3K3EHidefUM', -- Hash
     (
@@ -51,6 +55,7 @@ VALUES
   ),
   (
     'billing-service',
+    'billing-service_ecae3303e9ad8938',
     'FOOD_APP_BILLING_SERVICE_CLIENT_ID',
     '$argon2id$v=19$m=256,t=1,p=1$ZPcDajZJp9Q0D2dVNjtofw$+tG6xY1cq58bCrCWPSh0WeOO3f4Hz3me36a8CQDqm1A', -- Hash
     (

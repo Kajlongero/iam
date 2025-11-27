a12f86480c830c42f94ce0fec824f871e80cbbcb
Kajlongero <dev.kajlongero@gmail.com>
2025-11-27 12:31:56 -0400

    feat: deleting some unused cache keys

--- src/cache/providers/cache-keys.service.ts
+++ src/cache/providers/cache-keys.service.ts
@@ -11,45 +11,88 @@ export class CacheKeysService {
   }
 
   /**
-   * Find applications by their slug
+   * Find applications by their client id and retrieve their app slug
    */
-  getGlobalApplicationsLookup() {
-    return `iam:application:lookup`;
+  getGlobalApplicationsLookupKey() {
+    return `iam:application_lookup`;
   }
 
   /**
-   * Unique Application data cache key
-   * @param clientId {string}
+   * Key containing all mapped resource server client id to resource serve slug
    */
+  getGlobalRsLookupKey() {
+    return `iam:resource_servers_lookup`;
+  }
 
-  getApplicationsKey(clientId: string) {
-    return `iam:application:${clientId}`;
+  /**
+   * Key containing all mapped resource server client id to application slug
+   */
+  getGlobalApplicationSlugByRsClientIdLookupKey() {
+    return `iam:resource_servers_application_lookup`;
+  }
+
+  /**
+   * Key that contains a hash of resource servers information
+   * @param appSlug Application Unique Slug i.e IAM_CORE_0123456789abcdef
+   * @example
+   * "IAM_CORE_0123456789abcdef": {
+   *   "RESOURCE_SERVER_A": { ...RS_INFO }
+   * }
+   */
+  getApplicationResourceServersKey(appSlug: string) {
+    return `iam:application:${appSlug}:resource_servers`;
+  }
+
+  /**
+   * Key that contains a hash of resource servers plain names as their key and provides resource server slug
+   * @param appSlug Application Unique Slug i.e IAM_CORE_0123456789abcdef
+   */
+  getApplicationResourceServerLookupKey(appSlug: string) {
+    return `iam:application:${appSlug}:resource_servers_lookup`;
+  }
+
+  /**
+   * Retrieves the SET of permissions that a specific RS exposes/offers.
+   * Key: iam:application:{APP_ID}:resource_servers:{RS_ID}:exposed
+   * Value: Set<string> (['invoice:create', 'user:read'])
+   * @param appSlug Application client id
+   * @param rsSlug Resource Server client id
+   */
+  getApplicationResourceServerExposedPermissionsKey(
+    appSlug: string,
+    rsSlug: string
+  ) {
+    return `iam:application:${appSlug}:resource_servers:${rsSlug}:exposed`;
+  }
+
+  /**
+   * Retrieves the HASH of permissions that a Consumer is allowed to use on a Receptor.
+   * Key: iam:application:{APP_ID}:resource_servers:{RECEPTOR}:consumers:{CONSUMER}
+   * Value: Hash (Field: permission_name -> Value: rules_json)
+   */
+  getApplicationResourceServerConsumptionPermissionsKey(
+    appSlug: string,
+    receptorSlug: string,
+    consumerSlug: string
+  ) {
+    return `iam:application:${appSlug}:resource_servers:${receptorSlug}:consumers:${consumerSlug}`;
   }
 
   /**
    * Retrieve information from all Application cached roles key
-   * @param clientId Client id to search for
+   * @param appSlug Application unique slug to search for
    */
-  getApplicationsLocalRolesKey(clientId: string) {
-    return `iam:application:${clientId}:tenant:roles`;
-  }
-
-  /**
-   * Retrieve information from a specific cached role key
-   * @param clientId Client id to search for
-   * @param roleName Role name to retrieve specific info
-   */
-  getApplicationsLocalRoleByNameKey(clientId: string, roleName: string) {
-    return `iam:application:${clientId}:tenant:roles:${roleName}`;
+  getApplicationsLocalRolesKey(appSlug: string) {
+    return `iam:application:${appSlug}:roles`;
   }
 
   /**
    * Retrieve information of how the hierarchy of roles is on the Application key
-   * @param clientId Client id to search for
+   * @param appSlug App Slug to search for
    */
 
-  getApplicationsRoleHierarchyKey(clientId: string) {
-    return `iam:application:${clientId}:tenant:roles_hierarchy`;
+  getApplicationsRoleHierarchyKey(appSlug: string) {
+    return `iam:application:${appSlug}:roles:hierarchy`;
   }
 
   /**
@@ -60,39 +103,10 @@ export class CacheKeysService {
   }
 
   /**
-   * Retrieve information from a specific role of the global roles cache key
-   * @param roleName Role name to search for on the global roles cache key
-   */
-  getApplicationsGlobalRoleByNameKey(roleName: string) {
-    return `iam:application:global:roles:${roleName}`;
-  }
-
-  /**
    * Retrieve information of how the hierarchy of roles is on the global cached roles key
-   * @param clientId Client id to search for
    */
   getApplicationGlobalRolesHierarchyKey() {
-    return `iam:application:global:roles_hierarchy`;
-  }
-
-  /**
-   * Retrieve all the user permissions created (Such as permissions for users, permissions for admins, etc.) by one application key
-   * @param clientId Client id to search for
-   */
-  getApplicationsUserPermissionsKey(clientId: string) {
-    return `iam:application:${clientId}:user:permissions`;
-  }
-
-  /**
-   * Retrieve a specific user permission created by one application key
-   * @param clientId Client id to search for
-   * @param permission Permission name to search for
-   */
-  getApplicationsUserPermissionsByNameKey(
-    clientId: string,
-    permission: string
-  ) {
-    return `iam:application:${clientId}:user:permissions:${permission}`;
+    return `iam:application:global:roles:hierarchy`;
   }
 
   /**
@@ -124,116 +138,27 @@ export class CacheKeysService {
 
   /**
    * Retrieve all the API permissions created (Such as permissions for resource servers) by one application key
-   * @param clientId Client id to search for
+   * @param appSlug Application unique slug to search for
    */
-  getApplicationsApiPermissionsKey(clientId: string) {
-    return `iam:application:${clientId}:api:permissions`;
+  getApplicationsApiPermissionsKey(appSlug: string) {
+    return `iam:application:${appSlug}:api:permissions`;
   }
 
   /**
-   * Retrieve a specific api permission created by one application key
-   * @param clientId Client id to search for
-   * @param permission Permission name to search for
+   * Retrieve all the user permissions created (Such as permissions for users, permissions for admins, etc.) by one application key
+   * @param appSlug Application unique slug to search for
    */
-  getApplicationsApiPermissionsByNameKey(clientId: string, permission: string) {
-    return `iam:application:${clientId}:api:permissions:${permission}`;
+  getApplicationsUserPermissionsKey(appSlug: string) {
+    return `iam:application:${appSlug}:user:permissions`;
   }
-
-  // Application registered Objects and Methods
 
   /**
    * Retrieve a specific information from an object created by applications key
-   * @param clientId Client id to search for
+   * @param appSlug Application unique slug to search for
    * @param objectName objectName to search for
    */
-  getApplicationsObjectKey(clientId: string, objectName: string) {
-    return `iam:application:${clientId}:object:${objectName}`;
-  }
-
-  /**
-   * Retrieve meta object information from an object created by applications key
-   * @param clientId Client id to search for
-   * @param objectName objectName to search for
-   */
-  getApplicationsObjectMetaKey(clientId: string, objectName: string) {
-    return `iam:application:${clientId}:object:${objectName}:__meta`;
-  }
-
-  /**
-   * Retrieve a specific information from a method created by applications key
-   * @param clientId Client id to search for
-   * @param objectName Object to search for
-   * @param methodName Method to search for
-   */
-  getApplicationsObjectMethodKey(
-    clientId: string,
-    objectName: string,
-    methodName: string
-  ) {
-    return `iam:application:${clientId}:object:${objectName}:method:${methodName}`;
-  }
-
-  /**
-   * Retrieve global resource server their applicattionId by their clientId
-   */
-  getGlobalResourceServerLookupKey() {
-    return `iam:global:resource_server_names`;
-  }
-
-  /**
-   * Retrieve all the application registered resource servers cached key
-   * @param clientId
-   * @returns
-   */
-  getApplicationsResourceServersKey(clientId: string) {
-    return `iam:application:${clientId}:resource_servers`;
-  }
-
-  getApplicationsResourceServersNamesMappedKey(clientId: string) {
-    return `iam:application:${clientId}:resource_servers_names`;
-  }
-
-  getApplicationResourceServerSlugKey(clientId: string) {
-    return `iam:application:${clientId}:resource_servers_slugs`;
-  }
-
-  /**
-   * Retrieve unique application resource server information cached key
-   * @param clientId Client id to search for
-   * @param resourceServerClientId Resource Server to search for
-   */
-  getApplicationsResourceServerByClientIdKey(
-    clientId: string,
-    resourceServerClientId: string
-  ) {
-    return `iam:application:${clientId}:resource_servers:${resourceServerClientId}`;
-  }
-
-  /**
-   * Retrieves the SET of permissions that a specific RS exposes/offers.
-   * Key: iam:application:{APP_ID}:resource_servers:{RS_ID}:exposed
-   * Value: Set<string> (['invoice:create', 'user:read'])
-   * @param appClientId Application client id
-   * @param rsClientId Resource Server client id
-   */
-  getApplicationResourceServerExposedPermissionsKey(
-    appClientId: string,
-    rsClientId: string
-  ) {
-    return `iam:application:${appClientId}:resource_servers:${rsClientId}:exposed`;
-  }
-
-  /**
-   * Retrieves the HASH of permissions that a Consumer is allowed to use on a Receptor.
-   * Key: iam:application:{APP_ID}:resource_servers:{RECEPTOR}:consumers:{CONSUMER}
-   * Value: Hash (Field: permission_name -> Value: rules_json)
-   */
-  getApplicationResourceServerConsumptionPermissionsKey(
-    appClientId: string,
-    receptorClientId: string,
-    consumerClientId: string
-  ) {
-    return `iam:application:${appClientId}:resource_servers:${receptorClientId}:consumers:${consumerClientId}`;
+  getApplicationsObjectKey(appSlug: string, objectName: string) {
+    return `iam:application:${appSlug}:object:${objectName}`;
   }
 
   /**

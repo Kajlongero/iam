218562ec39fe07e98480b07c675089f1591fa29c
Kajlongero <dev.kajlongero@gmail.com>
2025-11-30 20:52:45 -0400

    feat: importing SecurityModule on TokenModule

--- src/token/token.module.ts
+++ src/token/token.module.ts
@@ -3,12 +3,13 @@ import { ConfigModule } from "@nestjs/config";
 
 import { CacheModule } from "src/cache/cache.module";
 import { CryptoModule } from "src/crypto/crypto.module";
+import { SecurityModule } from "src/security/security.module";
 
 import { TokenService } from "./token.service";
 import { TokenController } from "./token.controller";
 
 @Module({
-  imports: [ConfigModule, CacheModule, CryptoModule],
+  imports: [ConfigModule, CacheModule, CryptoModule, SecurityModule],
   providers: [TokenService],
   controllers: [TokenController],
 })

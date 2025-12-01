ac1dd16c9df200c0883fbe2305d3e7acbd832442
Kajlongero <dev.kajlongero@gmail.com>
2025-12-01 17:29:13 -0400

    feat: importing and exporting guards

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,13 +7,17 @@ import { CacheModule } from "src/cache/cache.module";
 import { SecurityModule } from "src/security/security.module";
 
 import { AuthService } from "./auth.service";
-
 import { AuthController } from "./auth.controller";
+
+import { AuthM2MGuard } from "./guards/m2m-auth.guard";
+import { AuthUserGuard } from "./guards/user-auth.guad";
+
+import { AuthGlobalGuard } from "./guards/global-auth.guard";
 
 @Module({
   imports: [ConfigModule, PrismaModule, CacheModule, SecurityModule],
-  exports: [AuthService],
-  providers: [AuthService],
+  exports: [AuthService, AuthM2MGuard, AuthUserGuard, AuthGlobalGuard],
+  providers: [AuthService, AuthM2MGuard, AuthUserGuard, AuthGlobalGuard],
   controllers: [AuthController],
 })
 export class AuthModule {}

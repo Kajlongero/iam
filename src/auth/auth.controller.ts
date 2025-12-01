8dcb8a36213241580c395673ff9310ba122e1e94
Kajlongero <dev.kajlongero@gmail.com>
2025-12-01 17:29:27 -0400

    feat: using authm2monly decorators

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,10 @@
+import { ConfigService } from "@nestjs/config";
+
 import { Controller, Post, Body, UseGuards } from "@nestjs/common";
 
 import { AuthService } from "./auth.service";
+
+import { AuthM2MOnly } from "src/security/decorators/m2m-only.decorator";
 
 import { BruteForceGuard } from "src/security/guards/brute-force.guard";
 
@@ -9,15 +13,20 @@ import { RegisterDto } from "./dtos/register-user.dto";
 
 @Controller("auth")
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private configService: ConfigService
+  ) {}
 
   @Post("login")
+  @AuthM2MOnly()
   @UseGuards(BruteForceGuard)
   login(@Body() payload: LoginDto) {
     return this.authService.login(payload);
   }
 
   @Post("register")
+  @AuthM2MOnly()
   register(@Body() payload: RegisterDto) {
     return this.authService.register(payload);
   }

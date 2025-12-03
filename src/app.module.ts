4e94f68da74b7629639ad313830cc86571125427
Kajlongero <dev.kajlongero@gmail.com>
2025-12-03 18:09:18 -0400

    feat: fixing imports order

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { APP_GUARD } from "@nestjs/core";
 import { ConfigModule } from "@nestjs/config";
 import { LoggerModule } from "nestjs-pino";
+import { IamConsistencyModule } from "./iam-consistency/iam-consistency.module";
 
 import { AuthModule } from "./auth/auth.module";
 import { CacheModule } from "./cache/cache.module";
@@ -9,8 +10,8 @@ import { TokenModule } from "./token/token.module";
 import { CryptoModule } from "./crypto/crypto.module";
 import { SecurityModule } from "./security/security.module";
 
+import { AuthGlobalGuard } from "./auth/guards/global-auth.guard";
 import { ValidateObjectMethodAccessGuard } from "./security/guards/validate-object-method-access.guard";
-import { IamConsistencyModule } from "./iam-consistency/iam-consistency.module";
 
 @Module({
   imports: [
@@ -24,6 +25,10 @@ import { IamConsistencyModule } from "./iam-consistency/iam-consistency.module";
     IamConsistencyModule,
   ],
   providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGlobalGuard,
+    },
     {
       provide: APP_GUARD,
       useClass: ValidateObjectMethodAccessGuard,

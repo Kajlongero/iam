cb8feb5528a0625930855610fcde4c63f6dec3d5
Kajlongero <dev.kajlongero@gmail.com>
2025-12-05 13:27:02 -0400

    feat: adding system sentinel role which does not do anything but their main role will be to act as a system protector to block certain actions (this role is the top of the hierarchy but does not have any permissions with)

--- database/seeds/07.platform.roles.sql
+++ database/seeds/07.platform.roles.sql
@@ -2,6 +2,16 @@ INSERT INTO
   access_control.roles (name, description, is_default, application_id)
 VALUES
   (
+    'SYSTEM SENTINEL',
+    'System Sentinel role which does not do anything, but stays over all roles due to a hierarchy position and will be used as a reference to block some actions',
+    FALSE,
+    NULL
+  );
+
+INSERT INTO
+  access_control.roles (name, description, is_default, application_id)
+VALUES
+  (
     'CORE',
     'Core Role for systems that can do everything within the IAM',
     FALSE,
@@ -43,6 +53,46 @@ INSERT INTO
   )
 VALUES
   (
+    'TRUSTED SYSTEM',
+    'Trusted System in our ecosystem where we can allow certain actions such as use generic login, register, etc...',
+    TRUE,
+    NULL,
+    (
+      SELECT
+        id
+      FROM
+        access_control.roles
+      WHERE
+        name = 'SUPER SYSTEM'
+        AND application_id IS NULL
+    )
+  );
+
+INSERT INTO
+  access_control.roles (
+    name,
+    description,
+    is_default,
+    application_id,
+    parent_role_id
+  )
+VALUES
+  (
+    'PREMIUM SYSTEM',
+    'Premium System role where that application can do more security related things',
+    TRUE,
+    NULL,
+    (
+      SELECT
+        id
+      FROM
+        access_control.roles
+      WHERE
+        name = 'TRUSTED SYSTEM'
+        AND application_id IS NULL
+    )
+  ),
+  (
     'BASIC SYSTEM',
     'Basic System role that has limited actions within the IAM',
     TRUE,
@@ -53,7 +103,7 @@ VALUES
       FROM
         access_control.roles
       WHERE
-        name = 'SUPER SYSTEM'
+        name = 'TRUSTED SYSTEM'
         AND application_id IS NULL
     )
   );
